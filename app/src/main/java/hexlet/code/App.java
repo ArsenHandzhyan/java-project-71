b9f5d744f4fa840d8c67da367dde804eccc10b63
arsen <arsenhan@mail.ru>
2023-10-25 16:26:19 +0300

    fix App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,7 +2,6 @@ package hexlet.code;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import picocli.CommandLine;
 
 import java.io.File;
