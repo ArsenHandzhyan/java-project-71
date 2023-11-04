72c33ed4f6aa784e2e67457f5cc8b887eb78480b
arsen <arsenhan@mail.ru>
2023-11-04 18:41:13 +0300

    fix App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,19 +6,53 @@ import picocli.CommandLine;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
-import java.util.TreeMap;
 import java.util.concurrent.Callable;
+import java.util.Iterator;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two JSON files and shows the differences.")
 public class App implements Callable<Integer> {
+    public static String filepath1;
+
+    static {
+        try {
+            filepath1 = getFile("/home/arsen/IdeaProjects/java-project-71/app/file1.json");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String filepath2;
+
+    static {
+        try {
+            filepath2 = getFile("/home/arsen/IdeaProjects/java-project-71/app/file2.json");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getFile(String pathFile) throws Exception {
+        Path path = Paths.get(pathFile).toAbsolutePath().normalize();
+        if (!Files.exists(path)) {
+            throw new Exception("File '" + path + "' does not exist");
+        }
+        return Files.readString(path);
+    }
+
     @CommandLine.Parameters(paramLabel = "file1.json", description = "path to the first JSON file")
-    File filepath1;
+    File filepath11;
 
     @CommandLine.Parameters(paramLabel = "file2.json", description = "path to the second JSON file")
-    File filepath2;
+    File filepath22;
+
     @CommandLine.Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
     private String format = "stylish";
 
@@ -32,14 +66,17 @@ public class App implements Callable<Integer> {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode json1 = mapper.readTree(filepath1);
         JsonNode json2 = mapper.readTree(filepath2);
+
         Map<String, String> diff = generateDifference(json1, json2, "");
+
         String diffOutput = generateDiffOutput(diff);
         System.out.println(diffOutput);
         return 0;
     }
 
+
     public static Map<String, String> generateDifference(JsonNode json1, JsonNode json2, String curPath) {
-        Map<String, String> diff = new TreeMap<>();
+        Map<String, String> diff = new HashMap<>();
         Iterator<String> fieldNames = json1.fieldNames();
 
         while (fieldNames.hasNext()) {
@@ -64,7 +101,6 @@ public class App implements Callable<Integer> {
                 diff.put("  " + path, json1.get(fieldName).asText());
             }
         }
-
         Iterator<String> remainingFieldNames = json2.fieldNames();
         while (remainingFieldNames.hasNext()) {
             String fieldName = remainingFieldNames.next();
@@ -74,7 +110,6 @@ public class App implements Callable<Integer> {
                 diff.put("+ " + path, json2.get(fieldName).asText());
             }
         }
-
         return diff;
     }
 
@@ -86,14 +121,24 @@ public class App implements Callable<Integer> {
     }
 
     public static String generateDiffOutput(Map<String, String> diff) {
+        List<Map.Entry<String, String>> sortedDiffEntries = new ArrayList<>(diff.entrySet());
+
+        // Сортируем список с использованием компаратора, игнорируя знаки и пробелы перед словами
+        sortedDiffEntries.sort((entry1, entry2) -> {
+            String key1 = entry1.getKey().replaceAll("[^a-zA-Z0-9]", "").trim();
+            String key2 = entry2.getKey().replaceAll("[^a-zA-Z0-9]", "").trim();
+            return key1.compareToIgnoreCase(key2);
+        });
+
         StringBuilder output = new StringBuilder("{\n");
 
-        for (Map.Entry<String, String> entry : diff.entrySet()) {
-            output.append("  ").append(entry.getKey());
-            output.append(": ");
-            output.append(entry.getValue());
-            output.append("\n");
+        // Формируем отсортированные строки
+        for (Map.Entry<String, String> entry : sortedDiffEntries) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            output.append(key).append(": ").append(value).append("\n");
         }
+
         output.append("}\n");
         return output.toString();
     }
