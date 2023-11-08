cd425e760a758cf402087f83afbcf6b1a66f110f
arsen <arsenhan@mail.ru>
2023-11-08 15:34:21 +0300

    fix AppTest.java

--- app/src/main/java/hexlet/code/StylishFormatter.java
+++ app/src/main/java/hexlet/code/StylishFormatter.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 public class StylishFormatter {
     public static String format(JsonNode node) {
         if (node.isObject() || node.isArray()) {
-            return node.toString().replaceAll("\"", "").replaceAll(",", ", ");
+            return node.toString().replaceAll("\"", "").replaceAll(",", ", ").replaceAll(":", "=");
         } else {
             return node.asText();
         }
