32b6e0411a785e2a74f8871efcc9470428afc726
arsen <arsenhan@mail.ru>
2023-11-13 01:39:56 +0300

    fix Parser class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,38 +1,46 @@
 package hexlet.code;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import hexlet.code.formatters.Formatter;
 import picocli.CommandLine;
-
 import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
-        description = "Compares two JSON files and shows the differences.")
-
+@CommandLine.Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two JSON or YAML files and shows the differences."
+)
 public class App implements Callable<Integer> {
 
-    @CommandLine.Parameters(paramLabel = "file1.json", description = "path to the first JSON file")
+    @CommandLine.Parameters(index = "0", description = "Path to the first file.")
     static File filepath1;
 
-    @CommandLine.Parameters(paramLabel = "file2.json", description = "path to the second JSON file")
+    @CommandLine.Parameters(index = "1", description = "Path to the second file.")
     static File filepath2;
 
-    @CommandLine.Option(names = {"-f", "--format"}, description = "Format out [default: stylish]")
+    @CommandLine.Option(names = {"-f", "--format"}, description = "Output format [default: stylish]")
     static String format = "stylish";
 
-    private final Parser yamlParser;
+    private final Parser parser;
 
     public App() {
-        this.yamlParser = new Parser();
+        this.parser = new Parser();
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 
     @Override
-    public Integer call() throws IOException {
-        JsonNode json1 = yamlParser.parseYaml(filepath1).orElseThrow(() -> new RuntimeException("File cannot be parsed"));
-        JsonNode json2 = yamlParser.parseYaml(filepath2).orElseThrow(() -> new RuntimeException("File cannot be parsed"));
+    public Integer call() {
+        JsonNode json1 = parser.parse(filepath1).orElseThrow(() -> new RuntimeException("File cannot be parsed: " + filepath1));
+        JsonNode json2 = parser.parse(filepath2).orElseThrow(() -> new RuntimeException("File cannot be parsed: " + filepath2));
         String diff = Differ.generate(json1, json2, format);
-        System.out.println(diff);
+        String formattedDiff = Formatter.formatterSelection(format, diff);
+        System.out.println(formattedDiff);
         return 0;
     }
 }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -3,25 +3,43 @@ package hexlet.code;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Parser {
+    private static final Logger LOGGER = Logger.getLogger(Parser.class.getName());
 
-    private final ObjectMapper objectMapper;
+    private final ObjectMapper yamlMapper;
+    private final ObjectMapper jsonMapper;
 
     public Parser() {
-        this.objectMapper = new ObjectMapper(new YAMLFactory());
+        this.yamlMapper = new ObjectMapper(new YAMLFactory());
+        this.jsonMapper = new ObjectMapper();
     }
 
-    public Optional<JsonNode> parseYaml(File file) {
+    public Optional<JsonNode> parse(File file) {
+        String extension = getFileExtension(file).orElse("");
         try {
-            return Optional.of(objectMapper.readTree(file));
+            if (extension.equals("json")) {
+                return Optional.of(jsonMapper.readTree(file));
+            } else if (extension.equals("yaml") || extension.equals("yml")) {
+                return Optional.of(yamlMapper.readTree(file));
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Problem parsing the file", e);
+        }
+        return Optional.empty();
+    }
+
+    private Optional<String> getFileExtension(File file) {
+        String name = file.getName();
+        int lastIndexOf = name.lastIndexOf(".");
+        if (lastIndexOf == -1) {
             return Optional.empty();
         }
+        return Optional.of(name.substring(lastIndexOf + 1));
     }
 }
