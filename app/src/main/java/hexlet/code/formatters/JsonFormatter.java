dee6512942cfd73818f39935ddcac3d83306abfa
arsen <arsenhan@mail.ru>
2023-11-22 23:46:51 +0300

    fix JsonFormatter

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -2,11 +2,8 @@ package hexlet.code.formatters;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import hexlet.code.Differ;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.IOException;
 
 public class JsonFormatter {
 
@@ -21,10 +18,5 @@ public class JsonFormatter {
             LOGGER.error("Error in processing JSON", e);
         }
         return jsonOutput.toString();
-    }
-    public static void main(String[] args) throws IOException {
-        String json1Path = "src/test/resources/fixtures/file1.json";
-        String json2Path = "src/test/resources/fixtures/file2.json";
-        System.out.println(Differ.generate(json1Path, json2Path, "stylish"));
     }
 }
