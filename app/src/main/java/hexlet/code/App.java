149fdb6e48f0dc4d47784967ca5c6a1d2cf7719b
arsen <arsenhan@mail.ru>
2023-11-30 16:51:15 +0300

    fix TreeBuilder and App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -13,14 +13,14 @@ import java.util.concurrent.Callable;
 )
 public class App implements Callable<Integer> {
     @CommandLine.Parameters(index = "0", arity = "1", description = "Path to the first file.")
-    private static String filepath1;
+    private String filepath1;
 
     @CommandLine.Parameters(index = "1", arity = "1", description = "Path to the second file.")
-    private static String filepath2;
+    private String filepath2;
 
     @CommandLine.Option(names = {"-f", "--format"}, defaultValue = "stylish",
             description = "Output format [default: stylish]")
-    private static String format;
+    private String format;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
--- app/src/main/java/hexlet/code/TreeBuilder.java
+++ app/src/main/java/hexlet/code/TreeBuilder.java
@@ -7,20 +7,33 @@ import java.util.TreeMap;
 public class TreeBuilder {
     public static Map<String, Object> buildTree(Map<String, Object> map1, Map<String, Object> map2) {
         Map<String, Object> difference = new TreeMap<>(new KeyComparator());
+
+        compareAndBuildDifference(map1, map2, difference);
+        addAdditionalEntries(map1, map2, difference);
+
+        return difference;
+    }
+
+    private static void compareAndBuildDifference(Map<String, Object> map1, Map<String, Object> map2, Map<String, Object> difference) {
         for (Map.Entry<String, Object> entry : map1.entrySet()) {
             String key = entry.getKey();
             Object value1 = formatStringValues(entry.getValue());
             Object value2 = formatStringValues(map2.get(key));
-            if (!map2.containsKey(key)) {
-                difference.put("- " + key, value1);
-            } else if (!(Objects.equals(value2, value1))) {
-                difference.put("- " + key, value1);
-                difference.put("+ " + key, value2);
+
+            if (!map2.containsKey(key) || !Objects.equals(value2, value1)) {
+                if (!map2.containsKey(key)) {
+                    difference.put("- " + key, value1);
+                } else {
+                    difference.put("- " + key, value1);
+                    difference.put("+ " + key, value2);
+                }
             } else {
                 difference.put("  " + key, value1);
             }
         }
+    }
 
+    private static void addAdditionalEntries(Map<String, Object> map1, Map<String, Object> map2, Map<String, Object> difference) {
         for (Map.Entry<String, Object> entry : map2.entrySet()) {
             String key = entry.getKey();
             Object value2 = formatStringValues(entry.getValue());
@@ -29,15 +42,9 @@ public class TreeBuilder {
                 difference.put("+ " + key, value2);
             }
         }
-        return difference;
     }
 
     public static Object formatStringValues(Object value) {
-        if (value instanceof String) {
-            return ("'" + value + "'");
-        } else {
-            return value;
-        }
+        return (value instanceof String) ? "'" + value + "'" : value;
     }
 }
-
