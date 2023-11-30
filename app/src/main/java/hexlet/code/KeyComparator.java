4c98019b9b9952902897885535749cda3df29ecb
arsen <arsenhan@mail.ru>
2023-11-30 06:11:31 +0300

    fix PlainFormatter

--- app/src/main/java/hexlet/code/KeyComparator.java
+++ app/src/main/java/hexlet/code/KeyComparator.java
@@ -2,16 +2,14 @@ package hexlet.code;
 
 import java.util.Comparator;
 
-public class KeyComparator implements Comparator<String> {
+public final class KeyComparator implements Comparator<String> {
     @Override
     public int compare(String key1, String key2) {
-        // Убираем знаки +, -, и пробелы при сравнении ключей
         String cleanKey1 = key1.replaceAll("[\\s\\-+]", "");
         String cleanKey2 = key2.replaceAll("[\\s\\-+]", "");
         if (cleanKey1.compareTo(cleanKey2) == 0) {
             return key2.compareTo(key1);
         }
-        // Сравниваем чистые ключи
         return cleanKey1.compareTo(cleanKey2);
     }
 }
