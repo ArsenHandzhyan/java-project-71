1aba2f1d304691fb4069b250413e8a6d7bd45245
arsen <arsenhan@mail.ru>
2023-12-05 16:38:08 +0300

    fix StylishFormatter.java

--- app/src/main/java/hexlet/code/formatters/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatters/StylishFormatter.java
@@ -1,9 +1,12 @@
 package hexlet.code.formatters;
 
+import hexlet.code.KeyComparator;
 import hexlet.code.OldAndNewValue;
 
 import java.util.Arrays;
 import java.util.Map;
+
+import static hexlet.code.TreeBuilder.*;
 
 public class StylishFormatter {
 
@@ -15,46 +18,43 @@ public class StylishFormatter {
             Object value = entry.getValue();
             String[] parts = key.split(" ");
             String updatedKey = parts[1];
-            if (key.startsWith("added")) {
-                formatted.append("  + ").append(updatedKey).append(": ");
-                formatted.append(getFormattedValue(value)).append("\n");
-            } else if (key.startsWith("removed")) {
-                formatted.append("  - ").append(updatedKey).append(": ");
-                formatted.append(getFormattedValue(value)).append("\n");
-            } else if (key.startsWith("updated")) {
+            if (key.startsWith(ADDITION)) {
+                formatted
+                        .append("  + ")
+                        .append(updatedKey)
+                        .append(": ")
+                        .append(getFormattedValue(value))
+                        .append(System.lineSeparator());
+            } else if (key.startsWith(DELETION)) {
+                formatted
+                        .append("  - ")
+                        .append(updatedKey)
+                        .append(": ")
+                        .append(getFormattedValue(value))
+                        .append(System.lineSeparator());
+            } else if (key.startsWith(CHANGE)) {
                 OldAndNewValue oldAndNewValue = (OldAndNewValue) value;
-                formatted.append("  - ").append(updatedKey).append(": ");
-                formatted.append(getFormattedValue(oldAndNewValue.getValue1())).append("\n");
-                formatted.append("  + ").append(updatedKey).append(": ");
-                formatted.append(getFormattedValue(oldAndNewValue.getValue2())).append("\n");
-            } else if (key.startsWith("unchanged")) {
-                formatted.append("    ").append(updatedKey).append(": ");
-                formatted.append(getFormattedValue(value)).append("\n");
+                formatted
+                        .append("  - ")
+                        .append(updatedKey)
+                        .append(": ")
+                        .append(getFormattedValue(oldAndNewValue.getValue1()))
+                        .append(System.lineSeparator())
+                        .append("  + ")
+                        .append(updatedKey)
+                        .append(": ")
+                        .append(getFormattedValue(oldAndNewValue.getValue2()))
+                        .append(System.lineSeparator());
+            } else if (key.startsWith(NOT_CHANGE)) {
+                formatted
+                        .append("    ")
+                        .append(updatedKey)
+                        .append(": ")
+                        .append(getFormattedValue(value))
+                        .append(System.lineSeparator());
             }
         }
-        String[] lines = formatted.toString().split(System.lineSeparator());
-        Arrays.sort(lines, (sb1, sb2) -> {
-            String[] parts1 = sb1.split(":");
-            String updatedKey1 = parts1[0];
-            String[] parts2 = sb2.split(":");
-            String updatedKey2 = parts2[0];
-            String cleanKey1 = updatedKey1.replaceAll("[\\s\\-+]", "");
-            String cleanKey2 = updatedKey2.replaceAll("[\\s\\-+]", "");
-            if (cleanKey1.compareTo(cleanKey2) == 0) {
-                return sb2.compareTo(sb1);
-            }
-            return cleanKey1.compareTo(cleanKey2);
-        });
-
-
-        // Собрать отсортированный результат обратно в StringBuilder
-        StringBuilder sortedSB = new StringBuilder("{\n");
-        for (String line : lines) {
-            sortedSB.append(line).append(System.lineSeparator());
-        }
-        sortedSB.append("}");
-
-        return sortedSB.toString();
+        return getSortedDiff(formatted);
     }
 
     private static String getFormattedValue(Object value) {
@@ -63,5 +63,11 @@ public class StylishFormatter {
         } else {
             return String.valueOf(value).replace("'", "");
         }
+    }
+
+    private static String getSortedDiff(StringBuilder formatted) {
+        String[] lines = formatted.toString().split(System.lineSeparator());
+        Arrays.sort(lines, new KeyComparator());
+        return String.join(System.lineSeparator(), lines);
     }
 }
