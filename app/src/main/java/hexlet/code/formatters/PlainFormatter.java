e11e57207311755514e78688874d1f9bac731c98
arsen <arsenhan@mail.ru>
2023-12-05 15:34:47 +0300

    fix PlainFormatter.java

--- app/src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -1,95 +1,62 @@
 package hexlet.code.formatters;
 
+import hexlet.code.OldAndNewValue;
+
+import java.util.Arrays;
 import java.util.Map;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class PlainFormatter {
+
     public static String format(Map<String, Object> diff) {
-        List<String> lines = new ArrayList<>();
-
+        StringBuilder formatted = new StringBuilder();
         for (Map.Entry<String, Object> entry : diff.entrySet()) {
-            String line = entry.getKey() + ": " + entry.getValue();
-            lines.add(line);
-        }
-        StringBuilder plainFormattedDiff = new StringBuilder();
-
-        for (int i = 0; i < lines.size(); ++i) {
-            String line = lines.get(i).trim();
-
-            if (shouldContinue(line)) {
-                continue;
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            String[] parts = key.split(" ");
+            String updatedKey = parts[1];
+            if (key.startsWith("added")) {
+                formatted.append(String.format("Property '%s' was added with value: %s%n",
+                        updatedKey, getFormattedValue1(value)));
+            } else if (key.startsWith("removed")) {
+                formatted.append(String.format("Property '%s' was removed%n", updatedKey));
+            } else if (key.startsWith("updated")) {
+                OldAndNewValue oldAndNewValue = (OldAndNewValue) value;
+                formatted.append(String.format("Property '%s' was updated. From %s to %s%n",
+                        updatedKey,
+                        getFormattedValue1(oldAndNewValue.getValue1()),
+                        getFormattedValue1(oldAndNewValue.getValue2())));
             }
-
-            i = handleLine(lines, plainFormattedDiff, i, line);
         }
-        return removeTrailingNewline(plainFormattedDiff.toString());
+        return getSortedDiff(formatted);
     }
 
-    private static boolean shouldContinue(String line) {
-        return line.isEmpty() || line.contains("{   ") || line.contains("}   ");
+    private static String getSortedDiff(StringBuilder formatted) {
+        String[] lines = formatted.toString().split(System.lineSeparator());
+        Arrays.sort(lines, (sb1, sb2) -> {
+            String[] parts1 = sb1.split(":");
+            String[] parts2 = sb2.split(":");
+            String updatedKey1 = parts1[0];
+            String updatedKey2 = parts2[0];
+            String cleanKey1 = updatedKey1.replaceAll("[\\s\\-+]", "");
+            String cleanKey2 = updatedKey2.replaceAll("[\\s\\-+]", "");
+            if (cleanKey1.compareTo(cleanKey2) == 0) {
+                return sb2.compareTo(sb1);
+            }
+            return cleanKey1.compareTo(cleanKey2);
+        });
+        return String.join(System.lineSeparator(), lines);
     }
 
-    private static int handleLine(List<String> lines, StringBuilder plainFormattedDiff, int i, String line) {
-        if (line.startsWith("-")) {
-            return handleRemovedProperty(lines, plainFormattedDiff, i);
-        } else if (line.startsWith("+")) { // Added properties
-            handleAddedProperty(plainFormattedDiff, line);
-        }
-        return i;
-    }
-
-    private static int handleRemovedProperty(List<String> lines, StringBuilder plainFormattedDiff, int i) {
-        String line = lines.get(i);
-        String property = extractProperty(line);
-        String oldValue = extractValue(line);
-        String formatOldValue = getFormattedValue(oldValue);
-
-        if (i < lines.size() - 1 && lines.get(i + 1).trim().startsWith("+")
-                && property.equals(extractProperty(lines.get(i + 1)))) {
-            String newValue = extractValue(lines.get(i + 1));
-            String formatNewValue = getFormattedValue(newValue);
-            plainFormattedDiff.append(String.format("Property '%s' was updated. From %s to %s%n",
-                    property, formatOldValue, formatNewValue));
-            i++;
+    private static String getFormattedValue1(Object value) {
+        if (Objects.isNull(value)) {
+            return null;
         } else {
-            plainFormattedDiff.append(String.format("Property '%s' was removed%n", property));
+            return isComplexValue(value.toString()) ? "[complex value]" : value.toString();
         }
-        return i;
     }
-
-    private static void handleAddedProperty(StringBuilder plainFormattedDiff, String line) {
-        String property = extractProperty(line);
-        String newValue = extractValue(line);
-        plainFormattedDiff.append(String.format("Property '%s' was added with value: %s%n",
-                property, getFormattedValue(newValue)));
-    }
-
-    private static String extractProperty(String line) {
-        return line.substring(1, line.indexOf(':')).trim().replaceAll("[^a-zA-Z0-9]", "");
-    }
-
-    private static String extractValue(String line) {
-        return line.substring(line.indexOf(':') + 1).replace("\"", "'").trim();
-    }
-
-    private static String getFormattedValue(Object value) {
-        return isComplexValue(value.toString()) ? "[complex value]" : value.toString();
-    }
-
 
     private static boolean isComplexValue(String value) {
         return value.startsWith("[") || value.startsWith("{");
-    }
-
-    private static String removeTrailingNewline(String string) {
-        String[] lines = string.split("\n");
-        StringBuilder builder = new StringBuilder();
-        for (String line : lines) {
-            if (!line.trim().isEmpty()) {
-                builder.append(line).append("\n");
-            }
-        }
-        return builder.toString().trim(); // this will automatically remove trailing newline character
     }
 }
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -10,7 +10,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static hexlet.code.Differ.generate;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public final class DifferTest {
 
