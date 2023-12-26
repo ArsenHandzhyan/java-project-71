074f7d27d1ba14e33558779cfc34184d86ace2b2
arsen <arsenhan@mail.ru>
2023-12-27 02:36:02 +0300

    fix Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,28 +1,67 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import hexlet.code.formatters.Formatter;
+import hexlet.code.formatters.UnsupportedFormatException;
+
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
 
 public final class Differ {
 
-    public static String generate(String filepath1, String filepath2) throws IOException {
+    public static String generate(String filepath1, String filepath2) {
         return generate(filepath1, filepath2, "stylish");
     }
 
-    public static String generate(String filepath1, String filepath2, String format) throws IOException {
-        Map<String, Object> mapJson1 = parseJsonOrYaml(filepath1);
-        Map<String, Object> mapJson2 = parseJsonOrYaml(filepath2);
-        Map<String, Object> diff = TreeBuilder.buildTree(mapJson1, mapJson2);
-        return Formatter.formatterSelection(format, diff);
+    public static String generate(String filepath1, String filepath2, String format) {
+        String content1;
+        String content2;
+        try {
+            content1 = readFileContent(filepath1);
+            content2 = readFileContent(filepath2);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        Map<String, Object> map1;
+        try {
+            map1 = parse(content1, getFileExtension(Path.of(filepath1)));
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+        Map<String, Object> map2;
+        try {
+            map2 = parse(content2, getFileExtension(Path.of(filepath2)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<Map<String, Object>> diff = TreeBuilder.buildTree(map1, map2);
+        try {
+            return Formatter.formatterSelection(format, diff); // This will also need refactoring to match new Formatter requirements.
+        } catch (JsonProcessingException | UnsupportedFormatException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private static Map<String, Object> parseJsonOrYaml(String filepath) throws IOException {
+    private static String readFileContent(String filepath) throws IOException {
         Path path = Paths.get(filepath);
-        Path file = resolvePath(path);
-        String fileExtension = getFileExtension(file);
-        return Parser.parse(fileExtension, file);
+        Path resolvedPath = resolvePath(path);
+        return Files.readString(resolvedPath);
+    }
+
+    private static Map<String, Object> parse(String content, String fileType) throws IOException {
+        if ("json".equalsIgnoreCase(fileType)) {
+            return Parser.parseJson(content);
+        } else if ("yml".equalsIgnoreCase(fileType) || "yaml".equalsIgnoreCase(fileType)) {
+            return Parser.parseYaml(content);
+        } else {
+            throw new IllegalArgumentException("Unsupported file type: " + fileType);
+        }
     }
 
     private static Path resolvePath(Path path) {
