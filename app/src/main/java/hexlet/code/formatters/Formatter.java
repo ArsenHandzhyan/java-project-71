9b0f4a0f00edcefa135fe505a87393bfe573fc43
arsen <arsenhan@mail.ru>
2023-12-27 02:35:54 +0300

    fix Formatter

--- app/src/main/java/hexlet/code/formatters/Formatter.java
+++ app/src/main/java/hexlet/code/formatters/Formatter.java
@@ -1,19 +1,17 @@
-package hexlet.code;
+package hexlet.code.formatters;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import hexlet.code.formatters.JsonFormatter;
-import hexlet.code.formatters.PlainFormatter;
-import hexlet.code.formatters.StylishFormatter;
 
+import java.util.List;
 import java.util.Map;
 
 public class Formatter {
-    public static String formatterSelection(String format, Map<String, Object> diff) throws JsonProcessingException {
+    public static String formatterSelection(String format, List<Map<String, Object>> diff) throws JsonProcessingException, UnsupportedFormatException {
         return switch (format.toLowerCase()) {
             case "stylish" -> StylishFormatter.format(diff);
             case "plain" -> PlainFormatter.format(diff);
             case "json" -> JsonFormatter.format(diff);
-            default -> "no such format";
+            default -> throw new UnsupportedFormatException(format);
         };
     }
 }
