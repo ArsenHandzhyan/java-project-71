59a24ccc0157239796cb08cffbd92fd13b35d934
arsen <arsenhan@mail.ru>
2023-12-27 04:25:37 +0300

    fix StylishFormatter

--- app/src/main/java/hexlet/code/formatters/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatters/StylishFormatter.java
@@ -9,48 +9,66 @@ import java.util.Map;
 public class StylishFormatter {
 
     public static String format(List<Map<String, Object>> diff) {
-        List<String> lines = new ArrayList<>();
-
-        for (Map<String, Object> change : diff) {
-            String changeType = change.keySet().stream()
-                    .filter(key -> key.matches("added|removed|updated|unchanged"))
-                    .findFirst()
-                    .orElseThrow(() -> new IllegalStateException("Invalid change type"));
-
-            String key = change.get(changeType).toString();
-            Object value = change.get("value");
-            Object oldValue = change.get("oldValue");
-            Object newValue = change.get("newValue");
-
-            switch (changeType) {
-                case "added":
-                    lines.add("  + " + key + ": " + getFormattedValue(value));
-                    break;
-                case "removed":
-                    lines.add("  - " + key + ": " + getFormattedValue(value));
-                    break;
-                case "updated":
-                    lines.add("  - " + key + ": " + getFormattedValue(oldValue));
-                    lines.add("  + " + key + ": " + getFormattedValue(newValue));
-                    break;
-                case "unchanged":
-                    lines.add("    " + key + ": " + getFormattedValue(value));
-                    break;
-                default:
-                    throw new IllegalArgumentException("Unknown change type: " + changeType);
-            }
-        }
-
+        List<String> lines = buildFormattedLines(diff);
         lines.sort(new KeyComparatorForStylishAndPlainFormats());
+        return buildFormattedOutput(lines);
+    }
 
+    private static List<String> buildFormattedLines(List<Map<String, Object>> diff) {
+        List<String> lines = new ArrayList<>();
+        diff.forEach(change -> lines.addAll(formatChange(change)));
+        return lines;
+    }
+
+    private static List<String> formatChange(Map<String, Object> change) {
+        List<String> formattedLines = new ArrayList<>();
+
+        String changeType = getChangeType(change);
+        String key = change.get(changeType).toString();
+        String formattedValue = getFormattedValue(change.get("value"));
+        String formattedOldValue = getFormattedValue(change.get("oldValue"));
+        String formattedNewValue = getFormattedValue(change.get("newValue"));
+
+        switch (changeType) {
+            case "added":
+                formattedLines.add(formatLine("  + ", key, formattedValue));
+                break;
+            case "removed":
+                formattedLines.add(formatLine("  - ", key, formattedValue));
+                break;
+            case "updated":
+                formattedLines.add(formatLine("  - ", key, formattedOldValue));
+                formattedLines.add(formatLine("  + ", key, formattedNewValue));
+                break;
+            case "unchanged":
+                formattedLines.add(formatLine("    ", key, formattedValue));
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown change type: " + changeType);
+        }
+        return formattedLines;
+    }
+
+    private static String getChangeType(Map<String, Object> change) {
+        return change.keySet().stream()
+                .filter(key -> key.matches("added|removed|updated|unchanged"))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Invalid change type"));
+    }
+
+    private static String formatLine(String prefix, String key, String value) {
+        return prefix + key + ": " + value;
+    }
+
+    private static String buildFormattedOutput(List<String> lines) {
         StringBuilder sortedSB = new StringBuilder("{\n");
         for (String line : lines) {
             sortedSB.append(line).append("\n");
         }
         sortedSB.append("}");
-
         return sortedSB.toString();
     }
+
 
     private static String getFormattedValue(Object value) {
         if (value instanceof Object[] || value instanceof List) {
