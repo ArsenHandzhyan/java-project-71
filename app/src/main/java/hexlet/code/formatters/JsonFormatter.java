3658d424d9495c8627d209344dd9a88f4f7d79e3
arsen <arsenhan@mail.ru>
2024-01-09 15:20:08 +0300

    fix JsonFormatter

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -2,23 +2,14 @@ package hexlet.code.formatters;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import hexlet.code.KeyComparatorForJsonFormat;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class JsonFormatter {
 
     public static String format(List<Map<String, Object>> diff) throws JsonProcessingException {
-        Map<String, Object> combinedMap = new LinkedHashMap<>();
-
-        diff.sort(new KeyComparatorForJsonFormat());
-        for (int i = 0; i < diff.size(); i++) {
-            combinedMap.put("change" + i, diff.get(i));
-        }
-
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(combinedMap);
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(diff);
     }
 }
