8593099021c3bae533125ae8d9d08238586e55fb
arsen <arsenhan@mail.ru>
2024-01-12 22:51:39 +0300

    fix PlainFormatter

--- app/src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -8,38 +8,33 @@ public class PlainFormatter {
     public static String format(List<Map<String, Object>> diff) {
         StringBuilder formatted = new StringBuilder();
         for (Map<String, Object> entry : diff) {
-            String name = "'" + entry.get("key") + "'";
-            String changeType = (String) entry.get("type");
-            String formattedValue = getFormattedValue(entry.get("value"));
-            String formattedOldValue = getFormattedValue(entry.get("oldValue"));
-            String formattedNewValue = getFormattedValue(entry.get("newValue"));
-            switch (changeType) {
-                case "added":
-                    formatted.append(String.format("Property %s was added with value: %s%n",
-                            name, formattedValue));
-                    break;
-                case "removed":
-                    formatted.append(String.format("Property %s was removed%n", name));
-                    break;
-                case "updated":
-                    formatted.append(String.format("Property %s was updated. From %s to %s%n",
-                            name, formattedOldValue, formattedNewValue));
-                    break;
-                case "unchanged": // don't use in this formatter
-                    break;
-                default:
-                    throw new IllegalArgumentException("Unknown change type: " + changeType);
-            }
+            formatted.append(formatEntry(entry));
         }
         return formatted.toString().trim();
     }
 
+    private static String formatEntry(Map<String, Object> entry) {
+        String name = "'" + entry.get("key") + "'";
+        String changeType = (String) entry.get("type");
+        String formattedValue = getFormattedValue(entry.get("value"));
+        String formattedOldValue = getFormattedValue(entry.get("oldValue"));
+        String formattedNewValue = getFormattedValue(entry.get("newValue"));
+
+        return switch (changeType) {
+            case "added" -> String.format("Property %s was added with value: %s%n",
+                    name, formattedValue);
+            case "removed" -> String.format("Property %s was removed%n",
+                    name);
+            case "updated" -> String.format("Property %s was updated. From %s to %s%n",
+                    name, formattedOldValue, formattedNewValue);
+            case "unchanged" -> ""; // don't use in this formatter
+            default -> throw new IllegalArgumentException("Unknown change type: " + changeType);
+        };
+    }
+
     private static String getFormattedValue(Object value) {
-        if (Objects.isNull(value)) {
-            return null;
-        } else {
-            return isComplexValue(value.toString()) ? "[complex value]" : value.toString();
-        }
+        return (Objects.isNull(value)) ? null : (isComplexValue(value.toString())
+                ? "[complex value]" : value.toString());
     }
 
     private static boolean isComplexValue(String value) {
